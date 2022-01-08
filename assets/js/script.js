0a2a816cb36f85dc12f008cea5f26e6589fe943c
Nate Boucher <natejboucher@gmail.com>
2022-01-08 11:48:48 -0500

    added city persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,46 @@
 cityName = $('#inputCity');
 
-var city = "";
+var city = [];
 
-function buttonHandler(value)    {
-    event.preventDefault();
+function createButtons() {
     $('#inputCity').val("");
-    var savedCity = $("<button>")
-    .addClass("btn btn-primary m-1 col-12")
-    .text(value);
-    $("#logCity").append(savedCity);
-    localStorage.setItem();
-    storeCity(value);
+    $('#logCity').empty();
+    for (var i = 0; i < city.length; i++) {
+        var savedCity = $("<button>")
+            .addClass("btn btn-primary m-1 col-12")
+            .text(city[i]);
+        $("#logCity").append(savedCity);
+    }
 };
 
 
-$('#submitSearch').on("click", function()  {
-    var cityInput = cityName.val();
-    buttonHandler(cityInput);
-    storedCity = {
-        city: cityInput
-    };
+$('#inputForm').on("submit", function (event) {
+    event.preventDefault();
+    if (cityName.val()) {
+        var cityInput = cityName.val();
+        city.push(cityInput);
+        saveCities();
+        createButtons();
+    } else {
+        alert("You must enter a valid city!");
+    }
 });
 
-function storeCity(data)  {
-    localStorage.setItem(city);
+function saveCities() {
+    localStorage.setItem("city", JSON.stringify(city));
 };
+
+function loadCities() {
+    city = JSON.parse(localStorage.getItem("city"));
+    if (!city)  {
+        city = [];
+    } else {
+    createButtons();
+    }
+};
+
+function loadCityCoord()  {
+    
+}
+
+loadCities();
